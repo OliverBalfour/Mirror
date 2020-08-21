de47e76fc5171efeddcec25122cfec41beed5970
Oliver Balfour <oliver.leo.balfour@gmail.com>
2020-08-21 16:02:43 +1000

    Add ESLint, minor changes

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 
 import { registerRootComponent } from 'expo';
 
-import Application from './src/main.js';
+import Application from './src/main';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
 registerRootComponent(Application);
+
+// Simple hack to customize the title because we cannot edit the HTML itself.
+try {
+  document.title = "Mirror App";
+} catch (e) {}
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
-import Button from './comps/button.js';
+import Button from './comps/button';
 
 const Application = () => (
   <PaperProvider>
